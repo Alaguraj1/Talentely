2fa4799b834a5ef21cb4d0a7c7fbc6163ed5a937
Alaguraj1 <alaguraj259@gmail.com>
2024-09-04 12:09:42 +0530

    changes

--- pages/404.js
+++ pages/404.js
@@ -3,23 +3,26 @@ import PageHead from "./Head";
 import Context from "@/context/Context";
 import Store from "@/redux/store";
 import { Provider } from "react-redux";
-
 import Separator from "@/components/Common/Separator";
 import FooterOne from "@/components/Footer/Footer-One";
 import HeaderStyleTen from "@/components/Header/HeaderStyle-Ten";
 import NotFound from "@/components/NotFound/NotFound";
+import HeaderStyleThree from "@/components/Header/HeaderStyle-Three";
+import FooterThree from "@/components/Footer/Footer-Three";
 
 const ErrorPage = () => {
   return (
     <Provider store={Store}>
       <Context>
         <PageHead title="Page not found - Online Courses & Education NEXTJS14 Template" />
-        <HeaderStyleTen headerSticky="rbt-sticky" headerType="" />
-
+        <HeaderStyleThree
+          headerType="rbt-transparent-header"
+          headerSticky="rbt-sticky"
+        />
         <NotFound />
 
         <Separator />
-        <FooterOne />
+        <FooterThree />
       </Context>
     </Provider>
   );
